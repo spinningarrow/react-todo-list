a0359ad6d4b53415467a10c9b0c4ab1420dddf64
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-20 22:20:14 +0800

    Simplify into one component file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,50 @@
-import React, { Component } from "react";
-import TodoList from "./TodoList";
+import React from "react";
+import { todos } from "../utils/seedData";
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    todos: todos,
+    newTodo: { description: "", isCompleted: false }
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const todos = [...this.state.todos, this.state.newTodo];
+
+    this.setState({
+      todos: todos,
+      newTodo: { description: "", isCompleted: false }
+    });
+  };
+
+  handleChange = event => {
+    this.setState({
+      newTodo: { description: event.target.value, isCompleted: false }
+    });
+  };
+
   render() {
-    return <TodoList title="jumpstart todo list" />;
+    return (
+      <div id="todo-list">
+        <h1 id="todo-title">So much to do!</h1>
+
+        {this.state.todos.map((todo, i) => {
+          return <li key={i}>{todo.description}</li>;
+        })}
+
+        <p>
+          Add todo:
+          <input
+            id="inputText"
+            type="text"
+            value={this.state.newTodo.description}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}>Submit</button>
+        </p>
+      </div>
+    );
   }
 }
 
